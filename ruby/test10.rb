274fdb8068947c3f1e0264681ca51a0ddf743f23
Katsumasa-Toda <katsumasatoda7@gmail.com>
2021-06-02 12:34:00 +0000

    [Update]test10

---
+++ ruby/test10.rb
@@ -0,0 +1,14 @@
+def findthird
+  num = 1
+  while (num <= 30 ) do
+    if (num % 3 == 0 || num.to_s.include?("3"))
+      puts "#{ num } ☆"
+    else
+      puts num
+    end
+
+    num = num + 1
+  end
+end
+
+findthird
